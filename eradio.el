87d9cfa2a72d793bdd579799797a313f56a3eca5
Olav Fosse <fosseolav@gmail.com>
2020-07-19 23:43:00 +0200

    Replace references to radio package with eradio

--- eradio.el
+++ eradio.el
@@ -1,10 +1,10 @@
-;;; radio.el --- A radio player                      -*- lexical-binding: t; -*-
+;;; eradio.el --- A radio player                      -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2020  Olav Fosse
 
 ;; Author: Olav Fosse <fosseolav@gmail.com>
 ;; Version: 0.1
-;; URL: https://github.com/olav35/radio
+;; URL: https://github.com/olav35/eradio
 ;; Package-Requires: ((emacs "24.1"))
 
 ;; This program is free software; you can redistribute it and/or modify
@@ -27,7 +27,7 @@
 ;;; Code:
 
 ;;;###autoload
-(defvar radio-channels '(("def con - soma fm" . "https://somafm.com/defcon256.pls")
+(defvar eradio-channels '(("def con - soma fm" . "https://somafm.com/defcon256.pls")
                          ("metal - soma fm" . "https://somafm.com/metal130.pls")
                          ("groove salad - soma fm" . "https://somafm.com/groovesalad256.pls")
                          ("secret agent - soma fm" . "https://www.somafm.com/secretagent.pls")
@@ -35,37 +35,37 @@
                          ("cafe - lainon" . "https://lainon.life:8000/cafe.ogg")
                          ("swing - lainon" . "https://lainon.life:8000/swing.ogg")
                          ("everything - lainon" . "https://lainon.life:8000/everything.ogg"))
-  "Radio's radio channels.")
+  "Eradio's radio channels.")
 
-(defvar radio-process nil "The process running the radio player.")
+(defvar eradio-process nil "The process running the radio player.")
 
-(defun radio-alist-keys (alist)
+(defun eradio-alist-keys (alist)
   "Get the keys from an ALIST."
   (mapcar #'car alist))
 
-(defun radio-stop ()
+(defun eradio-stop ()
   "Stop the radio player."
-  (interactive) (when radio-process (delete-process radio-process)))
+  (interactive) (when eradio-process (delete-process eradio-process)))
 
-(defun radio-play-low-level (URL)
+(defun eradio-play-low-level (URL)
   "Play radio channel URL in a new process."
-  (setq radio-process (start-process "radio-process" nil "vlc" "--no-video" "-I" "rc" URL)))
+  (setq eradio-process (start-process "eradio-process" nil "vlc" "--no-video" "-I" "rc" URL)))
 
-(defun radio-get-url ()
+(defun eradio-get-url ()
   "Get a radio channel URL from the user."
-  (let ((radio-channel (completing-read
+  (let ((eradio-channel (completing-read
                        "Channel: "
-                       (radio-alist-keys radio-channels)
+                       (eradio-alist-keys eradio-channels)
                        nil nil)))
-  (or (cdr (assoc radio-channel radio-channels)) radio-channel)))
+  (or (cdr (assoc eradio-channel eradio-channels)) eradio-channel)))
 
 ;;;###autoload
-(defun radio-play ()
+(defun eradio-play ()
   "Play a radio channel, do what I mean."
   (interactive)
-  (radio-stop)
-  (let ((url (radio-get-url)))
-    (radio-play-low-level url)))
+  (eradio-stop)
+  (let ((url (eradio-get-url)))
+    (eradio-play-low-level url)))
 
-(provide 'radio)
-;;; radio.el ends here
+(provide 'eradio)
+;;; eradio.el ends here
